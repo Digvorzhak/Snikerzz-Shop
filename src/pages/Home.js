b95aae3e41eaa0097f7fd97523f1a78052035a77
Digvorzhak <shon.h777@gmail.com>
2023-02-25 22:18:08 +0200

    project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import classes from "./Home.module.css";
 
 const HomePage = () => {
   return (
     <>
       <h1>Snikerzz Social Club</h1>
+      <img className={classes.landing} src="https://cdn.thewirecutter.com/wp-content/media/2021/02/whitesneakers-2048px-4187.jpg"></img>
     </>
   );
 };
